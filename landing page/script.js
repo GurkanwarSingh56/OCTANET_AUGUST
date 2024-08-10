142e3bd136633a83677f5271d5e5503b8fdf5609
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2024-08-10 14:34:07 +0530

    Add files via upload

---
+++ landing page/script.js
@@ -0,0 +1,29 @@
+const targetDate = new Date("23august, 2024 15:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = `
+        ${days}d ${hours}h ${minutes}m ${seconds}s
+    `;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerHTML = "The event has started!";
+    }
+}
+
+document.getElementById("feedback2").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    alert(name,email, message);
+});
